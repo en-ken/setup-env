b21ddf2b864f2378ab6857db49b95bb56847a87f
en-ken <inonakano.kawaz.taikai.never.knows@gmail.com>
2018-09-20 12:15:21 +0000

    add process to rewrite product.json to vscode installer

--- install-vscode-aarch64.sh
+++ install-vscode-aarch64.sh
@@ -4,6 +4,7 @@ echo ===INSTALL NECESSARY PACKAGES===
 export CC=/usr/bin/cc
 export CXX=/usr/bin/c++
 sudo apt install -y libsecret-1-dev git python2.7 clang make libx11-dev libxkbfile-dev fakeroot rpm
+sudo install -y jq
 npm install -g yarn
 
 echo ===CLONE VSCODE===
@@ -22,3 +23,17 @@ yarn run gulp vscode-linux-arm64-build-deb
 
 echo ===INSTALL VSCODE===
 sudo apt install -y ./.build/linux/deb/arm64/deb/code-oss_*_arm64.deb
+
+echo ===ADD EXTENSION GALLERY===
+gallery=$(cat << EOT
+{
+  "extensionsGallery":{
+    "serviceUrl": "https://marketplace.visualstudio.com/_apis/public/gallery",
+    "cacheUrl": "https://vscode.blob.core.windows.net/gallery/index",
+    "itemUrl": "https://marketplace.visualstudio.com/items"
+  }
+}
+EOT
+)
+jsonPath='/usr/share/code-oss/resources/app/product.json'
+cat ${jsonPath} | jq ". | .+ ${gallery}" >& ${jsonPath}
