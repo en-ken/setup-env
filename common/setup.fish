5d688310ec027688810a74940e79824fcee88c5f
en-ken <inonakano.kawaz.taikai.never.knows@gmail.com>
2018-09-22 00:14:55 +0900

    fix common setup.sh

---
+++ common/setup.fish
@@ -0,0 +1,6 @@
+#!/usr/bin/fish
+fisher omf/theme-l
+fisher edc/bass
+asdf plugin-add python
+asdf plugin-add nodejs
+bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
--- common/setup.sh
+++ common/setup.sh
@@ -1,42 +1,32 @@
 #!/bin/sh
-##package update
+echo == package update ==
 sudo sed -i'~' -E "s#http://(..\.)?(archive|security)\.ubuntu\.com/ubuntu#http://ftp.jaist.ac.jp/pub/Linux/ubuntu#g" /etc/apt/sources.list
-#sudo apt-add-repository -y ppa:git-core/ppa
 sudo apt update && sudo apt upgrade -y
 
-##standard package
+echo == standard package ==
+sudo apt install -y language-pack-ja
 sudo apt install -y git gcc make
 sudo apt install -y libssl-dev libbz2-dev libreadline-dev libsqlite3-dev zlib1g-dev automake autoconf libncurses-dev libyaml-dev libxslt-dev libffi-dev libtool unixodbc-dev gnupg
 
-##fish shell
+echo == fish shell ==
 sudo apt -y install fish
 fish -c ls #create config.fish
 
-##download fisherman
+echo == download fisherman ==
 curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisherman
 
-##download and set asdf
+echo == download and set asdf ==
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.5.1
 mkdir -p ~/.config/fish/completions
 cp ~/.asdf/completions/asdf.fish ~/.config/fish/completions
 
-##set env
+echo == set env ==
 cat << EOT >> ~/.config/fish/config.fish
 source ~/.asdf/asdf.fish
 EOT
 
-##configure under fish
-under_fish=$(cat << EOS
-fisher omf/theme-l
-fisher edc/bass
-asdf plugin-add python
-asdf plugin-add nodejs
-bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
-EOS
-)
-fish -c $under_fish
-
-
+echo == execute fish ==
+fish "`dirname $0`/setup.fish"
 
 ##disable beep
 echo "set bell-style none" >> ~/.inputrc
