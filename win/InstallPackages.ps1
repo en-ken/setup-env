c69df6fc277530cbf11d8728d44e4c150530949b
en-ken <inonakano.kawaz.taikai.never.knows@gmail.com>
2018-09-22 09:45:34 +0900

    fix InstallPackages.ps1

--- win/InstallPackages.ps1
+++ win/InstallPackages.ps1
@@ -8,23 +8,22 @@ cup -y all
 #Read package_list
 $list = "package_list.txt"
 $opt = $Args[0]
-if([string]::IsNullOrEmpty($opt)){
-    $opt = "common"
+if ([string]::IsNullOrEmpty($opt)) {
+    Write-Output "need args"
 }
 
 #Install packages
 $isTarget = $false
-Select-String -Pattern "\s*\S+" $list | ForEach-Object{
+Select-String -Pattern "\s*\S+" $list | ForEach-Object {
     #extract only necessary words
     $str = $_.Matches.Value
-    if($str -match "\[.+\]") 
-    {
+    if ($str -match "\[.+\]") {
         #common and selected segments are only installed.
         $isTarget = $str -match "(common|$opt)"
         return
     }
     
-    if($isTarget){
+    if ($isTarget) {
         choco install -y $str
     }
 }
