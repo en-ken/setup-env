325b4b0993401c58fbb34cf7d75e78553e8a339f
en-ken <inonakano.kawaz.taikai.never.knows@gmail.com>
2018-12-12 15:29:16 +0900

    refactor: clean scripts

--- common/setup.fish
+++ common/setup.fish
@@ -1,6 +1,45 @@
 #!/usr/bin/fish
-fisher omf/theme-l
-fisher edc/bass
+echo == install fisher ==
+curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+fish_update_completions
+
+echo == install standard packages ==
+fisher add jethrokuan/z
+fisher add edc/bass
+
+echo == install theme ==
+fisher add omf/theme-bobthefish
+string trim '
+set -g theme_color_scheme base16-light
+set -g theme_newline_cursor yes
+' >> ~/.config/fish/config.fish
+
+echo == install asdf ==
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.5.1
+mkdir -p ~/.config/fish/completions
+cp ~/.asdf/completions/asdf.fish ~/.config/fish/completions
+string trim 'source ~/.asdf/asdf.fish' >> ~/.config/fish/config.fish
+source ~/.config/fish/config.fish
 asdf plugin-add python
-asdf plugin-add nodejs
 bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+
+echo == install fzf ==
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+fisher add jethrokuan/fzf
+
+echo == intall go ==
+sudo apt install -y golang-go
+mkdir $HOME/go
+string trim '
+set -x GOPATH $HOME/go
+set -x PATH $GOPATH/bin $PATH
+' >> ~/.config/fish/config.fish
+source ~/.config/fish/config.fish
+
+go get github.com/motemen/ghq
+fisher add decors/fish-ghq
+
+echo == install node by fnm ==
+fisher add jorgebucaran/fnm
+fnm latest
--- common/setup.sh
+++ common/setup.sh
@@ -1,29 +1,25 @@
 #!/bin/sh
 echo == package update ==
 sudo sed -i'~' -E "s#http://(..\.)?(archive|security)\.ubuntu\.com/ubuntu#http://ftp.jaist.ac.jp/pub/Linux/ubuntu#g" /etc/apt/sources.list
-sudo apt update && sudo apt upgrade -y
+sudo apt update
+sudo apt upgrade -y
+
+echo == git ==
+sudo add-apt-repository -y ppa:git-core/ppa
+sudo apt update
+sudo apt install -y git
 
 echo == standard package ==
 sudo apt install -y language-pack-ja
-sudo apt install -y git gcc make
+sudo apt install -y gcc make
 sudo apt install -y libssl-dev libbz2-dev libreadline-dev libsqlite3-dev zlib1g-dev automake autoconf libncurses-dev libyaml-dev libxslt-dev libffi-dev libtool unixodbc-dev gnupg
 
 echo == fish shell ==
-sudo apt -y install fish
+sudo apt-add-repository -y ppa:fish-shell/release-2
+sudo apt update
+sudo apt install fish
+chsh -s `which fish`
 fish -c ls #create config.fish
-
-echo == download fisherman ==
-curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisherman
-
-echo == download and set asdf ==
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.5.1
-mkdir -p ~/.config/fish/completions
-cp ~/.asdf/completions/asdf.fish ~/.config/fish/completions
-
-echo == set env ==
-cat << EOT >> ~/.config/fish/config.fish
-source ~/.asdf/asdf.fish
-EOT
 
 echo == execute fish ==
 fish "`dirname $0`/setup.fish"
