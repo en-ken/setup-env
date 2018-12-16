2835e0e1880f58ad3ddf4befb2a953d68f53da0c
en-ken <inonakano.kawaz.taikai.never.knows@gmail.com>
2018-12-16 22:59:46 +0900

    fix: make vscode-installer together

---
+++ install-vscode/install-vscode-extensions.ps1
@@ -0,0 +1,24 @@
+echo === INSTALL EXTENSION ===
+code `
+--install-extension shakram02.bash-beautify `
+--install-extension coenraads.bracket-pair-colorizer `
+--install-extension peterjausovec.vscode-docker `
+--install-extension dbaeumer.vscode-eslint `
+--install-extension grapecity.gc-excelviewer `
+--install-extension eamodio.gitlens `
+--install-extension ms-ceintl.vscode-language-pack-ja `
+--install-extension orta.vscode-jest `
+--install-extension shd101wyy.markdown-preview-enhanced `
+--install-extension davidanson.vscode-markdownlint `
+--install-extension ibm.output-colorizer `
+--install-extension jebbs.plantuml `
+--install-extension ms-python.python `
+--install-extension chrmarti.regex `
+--install-extension mimarec.swagger-doc-viewer `
+--install-extension shuworks.vscode-table-formatter `
+--install-extension shardulm94.trailing-spaces `
+--install-extension eg2.tslint `
+--install-extension vscodevim.vim `
+--install-extension shardulm94.trailing-spaces `
+--install-extension chrmarti.regex `
+--install-extension eamodio.gitlens
