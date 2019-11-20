c8b19083cd88980e5ce71937d805a7acc6c2f1b6
en-ken <inonakano.kawaz.taikai.never.knows@gmail.com>
2019-11-20 12:08:16 +0900

    fix: updated docker script

--- linux/install-docker.sh
+++ linux/install-docker.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
+# setup docker-ce
 sudo apt install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
+apt-transport-https \
+ca-certificates \
+curl \
+gnupg-agent \
+software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+"deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update -y
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose
+sudo apt install -y docker-ce docker-ce-cli containerd.io
 
-sudo service docker start
+# setup docker-compose
+sudo apt install -y docker-compose
+sudo groupadd docker
+sudo gpasswd -a $USER docker
+
+# start docker daemon
+sudo systemctl start docker
+sudo systemctl enable docker
+
+echo "necessary to logout"
