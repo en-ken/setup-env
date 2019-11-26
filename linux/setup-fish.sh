ed94ff5e6269d34ee437d152c0a4e88e4870e9d2
en-ken <inonakano.kawaz.taikai.never.knows@gmail.com>
2019-11-26 18:00:42 +0900

    fix: instead of canging default shell, call fish from .bashrc

--- linux/setup-fish.sh
+++ linux/setup-fish.sh
@@ -5,7 +5,6 @@ echo == fish shell ==
 sudo apt-add-repository -y ppa:fish-shell/release-3
 sudo apt update
 sudo apt install fish
-chsh -s `which fish`
 fish -c ls #create config.fish
 
 echo == execute fish ==
